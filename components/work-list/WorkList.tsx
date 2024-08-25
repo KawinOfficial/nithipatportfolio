4c3feeb3ce06e75c07e0658cfba8ad0635bf2654
KawinSrisomphan <kawin@harmonyx.co>
2024-08-25 22:07:19 +0700

    fix: border work list color

--- components/work-list/WorkList.tsx
+++ components/work-list/WorkList.tsx
@@ -15,7 +15,7 @@ const WorkList = () => {
           <div key={`work-item-${item.label}`}>
             <Link
               href={item.link}
-              className="border block rounded-[16px] p-4 space-y-4"
+              className="border block rounded-[16px] p-4 space-y-4 hover:border-primary"
             >
               {item.image ? (
                 <div className="aspect-square relative">
